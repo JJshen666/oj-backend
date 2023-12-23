41ea621969f2e20783864423918e8c6a8c2ec446
Steven <1102520496@qq.com>
2023-12-23 20:43:20 +0800

    题目和题目提交开发

---
+++ src/main/java/com/shen/oj/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package com.shen.oj.mapper;
+
+import com.shen.oj.model.entity.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Steven
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @createDate 2023-12-19 22:58:19
+* @Entity com.shen.oj.model.entity.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
---
+++ src/main/java/com/shen/oj/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,18 @@
+package com.shen.oj.mapper;
+
+import com.shen.oj.model.entity.QuestionSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Steven
+* @description 针对表【question_submit(题目提交)】的数据库操作Mapper
+* @createDate 2023-12-19 23:00:36
+* @Entity com.shen.oj.model.entity.QuestionSubmit
+*/
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+
